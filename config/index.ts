34c3e4af40a25ac8e019b1e46715ee72a748d3c3
Jasonhfk <134176662+Jasonhfk@users.noreply.github.com>
2023-05-21 21:30:36 +0800

    Update index.ts

--- config/index.ts
+++ config/index.ts
@@ -1,5 +1,5 @@
 import { AppInfo } from "@/types/app"
-export const APP_ID = 'c530b09d-9671-476b-9a8c-99fcd6d680b4'
+export const APP_ID = ''
 export const API_KEY = 'sk-YGKZRvfbJWQuUQwO7kd47kIa9KCOI5yOuezveILH4TCGumer'
 export const API_URL = 'https://exapi-chat.zecoba.cn/v1/chat/completions'
 export const APP_INFO: AppInfo = {
